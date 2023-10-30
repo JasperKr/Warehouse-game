a6ac0c87f7ba25df40df463d5e5f3a27493ff1cd
Jasper <102589741+JasperKr@users.noreply.github.com>
2023-10-30 23:53:31 +0100

    glsl tutorial stuff

--- main.c
+++ main.c
@@ -1,16 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
 #include "glad.h"
 #define GLFW_INCLUDE_NONE
 #include "glfw3.h"
 
-int main() {
-
-    if (glfwInit()) {
-        printf("Init: Success");
-    } else {
-        printf("Init: Fail");
-    }
-
-    glfwTerminate();
+GLuint LoadShader(const char* shaderPath, GLenum shaderType) {
+    GLuint shaderID = glCreateShader(shaderType);
+    // Load shader source from the file
+    // Compile the shader
+    glShaderSource(shaderID, 1, &shaderSource, NULL);
+    glCompileShader(shaderID);
+    // Check for compilation errors (you can add error checking here)
+    // ...
+    return shaderID;
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}  
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+    if (!glfwInit())
+    {
+        printf("Error: Failed to initialize GLFW\n");
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1200, 800, "Warehouse", NULL, NULL);
+    if (!window)
+    {
+        printf("Error: Failed to create window\n");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        printf("Failed to initialize GLAD");
+        return -1;
+    }    
+    glViewport(0, 0, 1200, 800);
+    
+    // glsl tutorials
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };  
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);  
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    
+
+    glfwTerminate();
+    return 0;
+}
