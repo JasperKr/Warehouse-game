aee4a5414bfa837e445279f1414d1612f7e27afa
MethaneOceans <t-velema@outlook.com>
2023-10-31 15:39:02 +0100

    Square
    Code cleanup  time :'(

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #define GLFW_INCLUDE_NONE
 #include <glfw3.h>
 #include "main.h"
+#include <malloc.h>
+
 
 int main() {
-
 
     // Initialize GLFW
     if (glfwInit()) 
@@ -47,9 +48,15 @@ int main() {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+         0.5f,  0.5f,  0.0f,
+         0.5f, -0.5f,  0.0f,
+        -0.5f, -0.5f,  0.0f,
+        -0.5f,  0.5f,  0.0f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
     };
 
     unsigned int VBO;
@@ -57,6 +64,9 @@ int main() {
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
     // Vertex shader source and compilation
     const char *vertexShaderSource =    
@@ -66,6 +76,8 @@ int main() {
     "{\n"
     "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
+
+
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     
@@ -124,6 +136,9 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // Vertex attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -139,7 +154,7 @@ int main() {
         // Set the shaderprogram
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
