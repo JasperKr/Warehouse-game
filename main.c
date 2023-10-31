1ff1a5820f91de1f5d1f317a5366cfbc15a89d66
MethaneOceans <t-velema@outlook.com>
2023-10-31 17:45:34 +0100

    Pulling a TVA on the branch here

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #include "main.h"
 #include <malloc.h>
 
+char *readFile(char* file[]) {
+
+}
 
 int main() {
 
@@ -72,9 +75,11 @@ int main() {
     const char *vertexShaderSource =    
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "vertexColor = vec4(0.5, 0.5, 0.0, 1.0);\n"
     "}\0";
 
 
@@ -97,9 +102,10 @@ int main() {
     const char *fragmentShaderSource =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "FragColor = vec4 (1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "FragColor = vertexColor;\n"
     "}\0";
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
